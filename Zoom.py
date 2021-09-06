40e5680970b51d876cc9bf0608f1537df3813388
Ilia <istudyatuni@gmail.com>
2021-09-06 21:05:03 +0300

    Refactor:
    split to functions
    run zoom not only on windows (also use APPDATA env variable)

--- Zoom.py
+++ Zoom.py
@@ -3,52 +3,93 @@ import pandas as pd
 from datetime import datetime
 import pyautogui
 
-#reading the meeting details
+# reading the meeting details
 df = pd.read_csv('meetingschedule.csv')
 df_new = pd.DataFrame()
 
-while(True):
-    #Check the current system time
-    timestr = datetime.now().strftime("%H:%M")
-    #Check if the current time is mentioned in the Dataframe
-    if timestr in df.Time.values:
-        df_new = df[df['Time'].astype(str).str.contains(timestr)]
-        #Open the Zoom app
-        subprocess.Popen("C:\\Users\\ZEESHAN\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
-        time.sleep(15)
-        #Locate the position of the join button on the screen
-        position = pyautogui.locateOnScreen("buttons\\join_button.png")
-        #Move the cursor to the position of the button
-        pyautogui.moveTo(position)
-        #Perform click operation
-        pyautogui.click()
-        time.sleep(2)
-        #Write the meeting ID from the dataframe onto the Zoom App
-        keyboard.write(df_new.iloc[0,1])
+def open_zoom():
+    import sys, os
+    if sys.platform.startswith('win32'):
+        appdata = os.getenv('APPDATA')
+        subprocess.Popen(f'{appdata}/Zoom/bin/Zoom.exe')
+    else:
+        subprocess.Popen('zoom')
 
-        #For tapping the Turn off video option on Zoom app
-        position = pyautogui.locateOnScreen("buttons\\turn_off_vid_button.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
-        time.sleep(2)
+    time.sleep(3)
 
-        #For tapping on the Join button
-        position = pyautogui.locateOnScreen("buttons\\join_button_2.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
-        time.sleep(7)
+def click_button_by_image(name):
+    # Locate the position of the button on the screen
+    position = pyautogui.locateOnScreen(name)
 
-        #Reads the Meeting Passcode from the dataframe and enters into the zoom app
-        keyboard.write(str(int(df_new.iloc[0,2])))
-        time.sleep(3)
+    if not position:
+        return False
 
-        #For finally joining the meeting
-        position = pyautogui.locateOnScreen("buttons\\join_meeting.png")
-        pyautogui.moveTo(position)
-        pyautogui.click()
+    # Move the cursor to the position of the button
+    pyautogui.moveTo(position)
+    # Perform click operation
+    pyautogui.click()
 
-        #Wait for one minute before the next iteration starts
-        time.sleep(60)
+    return True
+
+def click_join_button_with_plus():
+    result = click_button_by_image('buttons/join_button.png')
+
+    if not result:
+        # i don't know why, but on linux here is different button
+        click_join_meeting_button()
+        return
+
+    time.sleep(2)
+
+def click_join_button_with_text():
+    # For tapping on the Join button
+    click_button_by_image('buttons/join_button_2.png')
+
+    time.sleep(7)
+
+def click_join_meeting_button():
+    click_button_by_image('buttons/join_meeting.png')
+
+def click_turn_off_video():
+    # For tapping the Turn off video option on Zoom app
+    click_button_by_image('buttons/turn_off_vid_button.png')
+
+    time.sleep(2)
+
+def write_from_keyboard(text):
+    # Write the meeting ID from the dataframe onto the Zoom App
+    keyboard.write(text)
+
+def main():
+    # Check the current system time
+    timestr = datetime.now().strftime('%H:%M')
+
+    # Check if the current time is mentioned in the Dataframe
+    if timestr not in df.Time.values:
+        return
+
+    df_new = df[df['Time'].astype(str).str.contains(timestr)]
+
+    open_zoom()
+    click_join_button_with_plus()
+
+    # write ID
+    write_from_keyboard(df_new.iloc[0,1])
+
+    click_turn_off_video()
+
+    click_join_button_with_text()
 
 
+    # Reads the Meeting Passcode from the dataframe and enters into the zoom app
+    write_from_keyboard(str(int(df_new.iloc[0,2])))
+    time.sleep(3)
 
+    click_join_meeting_button()
+
+    # Wait for one minute before the next iteration starts
+    time.sleep(60)
+
+if __name__ == '__main__':
+    while(True):
+        main()
