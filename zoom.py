01c002edf5f81591f32ffda1cf3ee1d60580cbf6
Ilia <istudyatuni@gmail.com>
2021-09-07 21:07:24 +0300

    Improvements

--- zoom.py
+++ zoom.py
@@ -5,47 +5,49 @@ from datetime import datetime
 
 # https://superuser.com/a/1563359
 # https://superuser.com/a/1624394
-join_url = '--url="zoommtg://zoom.us/join?confno={meeting_id}&pwd={hashed_pass}"'
+join_url = '--url="zoommtg://zoom.us/join?confno={meeting_id}&pwd={pwd}"'
 
+
+silent_log = False
+
+def simple_log(*args):
+    if not silent_log:
+        print(*args)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Zoom start automation')
 
     parser.add_argument('-c', '--cron', action='store_true', help='Script is running with crontab. Also can be used for running script once')
     parser.add_argument('-s', '--schedule', type=str, help='File with schedule', default='meetingschedule.csv')
+    parser.add_argument('-q', '--quite', action='store_true', help='Be more quite')
 
     return parser.parse_args()
 
-def zoom_exec():
+def get_zoom_exec():
     import sys
     if sys.platform.startswith('win32'):
         # %HOME%/Appdata/Roaming
         appdata = os.getenv('APPDATA')
         zoom_exe = f'{appdata}/Zoom/bin/Zoom.exe'
 
-        if not os.path.exists(zoom_exe):
-            zoom_exe = ''
+        if os.path.exists(zoom_exe):
+            return zoom_exe
     else:
         # linux, unix, etc (I hope)
-        zoom_exe = 'zoom'
+        if os.system('which zoom') == 0:
+            return 'zoom'
 
-        if os.system('which zoom') == 256:
-            zoom_exe = ''
-
-    if zoom_exe:
-        return zoom_exe
-
-    quit('zoom not installed or not found')
+    quit('zoom is not installed or not found')
 
 def open_zoom(meeting_id, hashed_pass):
     join_param = join_url.format(
         meeting_id=meeting_id.replace(' ', ''),
-        hashed_pass=hashed_pass,
+        pwd=hashed_pass,
     )
 
-    run_url = f'{zoom_exec()} {join_param}'
+    run_url = f'{get_zoom_exec()} {join_param}'
 
-    print('run:', run_url)
+    simple_log('run:', run_url)
     os.system(run_url)
 
 def check_zoom_running():
@@ -57,16 +59,19 @@ def main(schedule_file):
     with open(schedule_file) as csv_file:
         meetings = list(csv.DictReader(
             csv_file,
+            # rename columns
             fieldnames=('time', 'meeting_id', 'pwd'),
         ))
 
     # Check the current system time
     timestr = datetime.now().strftime('%H:%M')
 
-    current = next(a for a in meetings if a['time'] == timestr)
-
-    # Check if the current time is mentioned in schedule
-    if not current:
+    try:
+        # get first meeting with current time
+        current = next(a for a in meetings if a['time'] == timestr)
+    except StopIteration:
+        # current time is not mentioned in schedule
+        simple_log('no meeting at', timestr)
         return
 
     open_zoom(current['meeting_id'], current['pwd'])
@@ -76,6 +81,7 @@ if __name__ == '__main__':
         quit('Another instance of zoom is running. Quitting')
 
     args = parse_args()
+    silent_log = args.quite
 
     if args.cron:
         main(args.schedule)
